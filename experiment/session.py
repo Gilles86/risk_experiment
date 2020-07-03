e47bf9b32ffdff6afed15b53c71fff4ea120de8d
Anjali Beharelle <anjali.rajabeharelle@econ.uzh.ch>
2020-07-03 08:16:55 +0200

    Some small tweaks in SNS lab

--- experiment/session.py
+++ experiment/session.py
@@ -1,12 +1,12 @@
 from stimuli import FixationLines
-from exptools2.core import Session
+from exptools2.core import Session, PylinkEyetrackerSession
 from psychopy import visual
 
-class PileSession(Session):
+class PileSession(PylinkEyetrackerSession):
     """ Simple session with x trials. """
-    def __init__(self, output_str, subject=None, output_dir=None, settings_file=None):
+    def __init__(self, output_str, subject=None, output_dir=None, settings_file=None, eyetracker_on=True):
         """ Initializes TestSession object. """
-        super().__init__(output_str, output_dir=None, settings_file=settings_file)
+        super().__init__(output_str, output_dir=None, settings_file=settings_file, eyetracker_on=eyetracker_on)
         self.subject = subject
 
         print(self.settings)
@@ -22,7 +22,9 @@ class PileSession(Session):
 
     def run(self):
         """ Runs experiment. """
+        self.calibrate_eyetracker()
         self.start_experiment()
+        self.start_recording_eyetracker()
         for trial in self.trials:
             trial.run()
 
