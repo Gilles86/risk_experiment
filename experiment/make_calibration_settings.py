fa7f079927d2e0f433c3ca9c820b54df2aa0e022
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2020-07-07 13:01:25 +0200

    Seperate folder for calibration settings

--- experiment/make_calibration_settings.py
+++ experiment/make_calibration_settings.py
@@ -5,6 +5,7 @@ from itertools import product
 import os
 import os.path as op
 
+
 def main(subject):
     N_RUNS = 3
 
@@ -18,37 +19,46 @@ def main(subject):
 
     df = []
     for ix, p in enumerate(prob1):
-        tmp = pd.DataFrame(product(base, fractions), columns=['base number', 'fraction'])
+        tmp = pd.DataFrame(product(base, fractions), columns=[
+                           'base number', 'fraction'])
         tmp['p1'] = p
         tmp['p2'] = prob2[ix]
-        
+
         df.append(tmp)
-        
+
     df = pd.concat(df).reset_index(drop=True)
 
     df.loc[df['p1'] == 1.0, 'n1'] = df['base number']
     df.loc[df['p1'] != 1.0, 'n2'] = df['base number']
-    df.loc[df['p1'] == 1.0, 'n2'] = (df['fraction'] * df['base number']).astype(int)
-    df.loc[df['p1'] != 1.0, 'n1'] = (df['fraction'] * df['base number']).astype(int)
+    df.loc[df['p1'] == 1.0, 'n2'] = (
+        df['fraction'] * df['base number']).astype(int)
+    df.loc[df['p1'] != 1.0, 'n1'] = (
+        df['fraction'] * df['base number']).astype(int)
 
     df['n1'] = df['n1'].astype(int)
     df['n2'] = df['n2'].astype(int)
 
     # Shuffle _within_ p1's
-    df = df.groupby('p1', as_index=False).apply(lambda d: d.sample(frac=1)).reset_index(level=0, drop=True)
+    df = df.groupby('p1', as_index=False).apply(
+        lambda d: d.sample(frac=1)).reset_index(level=0, drop=True)
 
     # Get run numbers
-    df['run'] = df.groupby('p1').p1.transform(lambda p: np.ceil((np.arange(len(p))+1) / (len(p) / N_RUNS))).astype(int)
+    df['run'] = df.groupby('p1').p1.transform(lambda p: np.ceil(
+        (np.arange(len(p))+1) / (len(p) / N_RUNS))).astype(int)
 
     df = df.set_index(['run', 'p1'])
     ixs = np.random.permutation(df.index.unique())
-    df = df.loc[ixs].sort_index(level='run', sort_remaining=False).reset_index('p1')
+    df = df.loc[ixs].sort_index(
+        level='run', sort_remaining=False).reset_index('p1')
 
-    if not op.exists(op.abspath('settings')):
-        os.makedirs(op.abspath('settings'))
+    calibrate_settings_folder = op.abspath(op.join('settings', 'calibration'))
+    if not op.exists(calibrate_settings_folder):
+        os.makedirs(calibrate_settings_folder)
 
     df['trial'] = np.arange(1, len(df)+1)
-    df.to_csv(op.abspath(op.join('settings', f'sub-{subject}_ses-calibrate.tsv')), sep='\t')
+    df.to_csv(op.abspath(op.join(calibrate_settings_folder,
+                                 f'sub-{subject}_ses-calibrate.tsv')), sep='\t')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
