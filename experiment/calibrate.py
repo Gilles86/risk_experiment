c879d109aca18663e6ccb818be78df28595036da
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2020-07-07 11:38:22 +0200

    Store stimuli locations for calibration script

--- experiment/calibrate.py
+++ experiment/calibrate.py
@@ -3,7 +3,7 @@ from exptools2.core import Trial
 from psychopy.visual import Pie, TextStim
 from trial import InstructionTrial
 from session import PileSession
-from utils import run_experiment
+from utils import run_experiment, create_stimulus_array_log_df
 from stimuli import _create_stimulus_array, CertaintyStimulus, ProbabilityPieChart
 import numpy as np
 from psychopy import logging
@@ -13,8 +13,8 @@ import pandas as pd
 
 class IntroBlockTrial(Trial):
 
-    def __init__(self, session, trial_nr, phase_durations=[5.], 
-            prob1=0.55, prob2=1.0, **kwargs):
+    def __init__(self, session, trial_nr, phase_durations=[5.],
+                 prob1=0.55, prob2=1.0, **kwargs):
         super().__init__(session, trial_nr, phase_durations, **kwargs)
 
         txt = f"""
@@ -30,13 +30,14 @@ class IntroBlockTrial(Trial):
         piechart_pos1 = .5 * -text_width - .25 * piechart_width, 1.5 * piechart_width
         piechart_pos2 = .5 * -text_width - .25 * piechart_width, -1.5 * piechart_width
 
-        self.piechart1 = ProbabilityPieChart(self.session.win, prob1, pos=piechart_pos1, size=piechart_width)
-        self.piechart2 = ProbabilityPieChart(self.session.win, prob2, pos=piechart_pos2, size=piechart_width)
+        self.piechart1 = ProbabilityPieChart(
+            self.session.win, prob1, pos=piechart_pos1, size=piechart_width)
+        self.piechart2 = ProbabilityPieChart(
+            self.session.win, prob2, pos=piechart_pos2, size=piechart_width)
 
-        self.text = TextStim(self.session.win, txt, 
-                wrapWidth=text_width,
-                height=text_height)
-
+        self.text = TextStim(self.session.win, txt,
+                             wrapWidth=text_width,
+                             height=text_height)
 
     def draw(self):
         self.text.draw()
@@ -45,15 +46,15 @@ class IntroBlockTrial(Trial):
 
 
 class GambleTrial(Trial):
-    def __init__(self, session, trial_nr, phase_durations=None, 
-            prob1=0.55, prob2=1.0, num1=10, num2=5,
-            jitter1=2.5, jitter2=4.0, **kwargs):
-
+    def __init__(self, session, trial_nr, phase_durations=None,
+                 prob1=0.55, prob2=1.0, num1=10, num2=5,
+                 jitter1=2.5, jitter2=4.0, **kwargs):
 
         if phase_durations is None:
-            phase_durations = [.3, .5, .6, jitter1, .3, .5, .6, jitter2] 
+            phase_durations = [.3, .5, .6, jitter1, .3, .5, .6, jitter2]
         else:
-            raise Exception("Don't directly set phase_durations for GambleTrial!")
+            raise Exception(
+                "Don't directly set phase_durations for GambleTrial!")
 
         super().__init__(session, trial_nr, phase_durations, **kwargs)
 
@@ -66,32 +67,38 @@ class GambleTrial(Trial):
         self.buttons = self.session.settings['various'].get('buttons')
         piechart_width = self.session.settings['various'].get('piechart_width')
 
-        self.piechart1 = ProbabilityPieChart(self.session.win, prob=prob1, size=piechart_width)
-        self.piechart2 = ProbabilityPieChart(self.session.win, prob=prob2, size=piechart_width)
+        self.piechart1 = ProbabilityPieChart(
+            self.session.win, prob=prob1, size=piechart_width)
+        self.piechart2 = ProbabilityPieChart(
+            self.session.win, prob=prob2, size=piechart_width)
 
         # self.text1 = TextStim('Pie1: {int(
 
         self.pile1 = _create_stimulus_array(self.session.win,
-            num1,
-            self.session.settings['pile'].get('aperture_radius'),
-            self.session.settings['pile'].get('dot_radius'),
-            image=self.session.image1)
+                                            num1,
+                                            self.session.settings['pile'].get(
+                                                'aperture_radius'),
+                                            self.session.settings['pile'].get(
+                                                'dot_radius'),
+                                            image=self.session.image1)
 
         self.pile2 = _create_stimulus_array(self.session.win,
-            num2,
-            self.session.settings['pile'].get('aperture_radius'),
-            self.session.settings['pile'].get('dot_radius'),
-            image=self.session.image1)
-
+                                            num2,
+                                            self.session.settings['pile'].get(
+                                                'aperture_radius'),
+                                            self.session.settings['pile'].get(
+                                                'dot_radius'),
+                                            image=self.session.image1)
+        self.stimulus_arrays = [self.pile1, self.pile2]
 
         self.choice_stim = TextStim(self.session.win)
         button_size = self.session.settings['various'].get('button_size')
-        self.certainty_stim = CertaintyStimulus(self.session.win, response_size=[button_size, button_size])
+        self.certainty_stim = CertaintyStimulus(
+            self.session.win, response_size=[button_size, button_size])
 
         self.choice = None
         self.certainty = None
         self.certainty_time = np.inf
-
 
     def draw(self):
         self.session.fixation_lines.draw()
@@ -112,8 +119,6 @@ class GambleTrial(Trial):
                 else:
                     if (self.session.clock.getTime() - self.certainty_time) < .5:
                         self.certainty_stim.draw()
-
-
 
     def get_events(self):
         events = super().get_events()
@@ -142,17 +147,16 @@ class GambleTrial(Trial):
 
     def get_stimulus_array_log(self):
 
-        n_repeats = len(self.colors)
-        n_dots = self.parameters['n_dots']
+        n_dots1 = self.parameters['n1']
+        n_dots2 = self.parameters['n2']
 
-        # trial_ix, stim_array, color, stimulus_ix
-        trial_ix = np.ones(n_repeats * n_dots) * self.trial_nr
-        array_ix = np.repeat(np.arange(n_repeats) + 1, n_dots)
-        color_ix = np.repeat(self.colors, n_dots)
-        stim_ix = np.tile(np.arange(n_dots) + 1, n_repeats)
+        # trial_ix, stim_array, stimulus_ix
+        trial_ix = np.ones(n_dots1 + n_dots2) * self.trial_nr
+        array_ix = [1] * n_dots1 + [2] * n_dots2
+        stim_ix = np.hstack((np.arange(n_dots1) + 1, np.arange(n_dots2)+1))
 
-        index = pd.MultiIndex.from_arrays([trial_ix, array_ix, color_ix, stim_ix],
-                                          names=('trial_nr', 'array_nr', 'color', 'stim_nr'))
+        index = pd.MultiIndex.from_arrays([trial_ix, array_ix, stim_ix],
+                                          names=('trial_nr', 'array_nr', 'stim_nr'))
 
         log = create_stimulus_array_log_df(self.stimulus_arrays, index=index)
 
@@ -166,7 +170,8 @@ class GambleTrial(Trial):
             self.session.global_log.loc[idx, 'trial_nr'] = self.trial_nr
             self.session.global_log.loc[idx, 'onset'] = onset
             self.session.global_log.loc[idx, 'phase'] = self.phase
-            self.session.global_log.loc[idx, 'nr_frames'] = self.session.nr_frames
+            self.session.global_log.loc[idx,
+                                        'nr_frames'] = self.session.nr_frames
 
         if choice is not None:
             self.session.global_log.loc[idx, 'event_type'] = 'choice'
@@ -175,9 +180,9 @@ class GambleTrial(Trial):
             self.session.global_log.loc[idx, 'event_type'] = 'certainty'
             self.session.global_log.loc[idx, 'certainty'] = certainty
 
+
 class CalibrationSession(PileSession):
 
-    
     Trial = GambleTrial
 
     def create_trials(self):
@@ -189,7 +194,7 @@ class CalibrationSession(PileSession):
         In this task, you will see two piles of swiss Franc coins in
         succession. Both piles are combined with a pie chart in.
         The part of the pie chart that is lightly colored indicates
-        the probability of a lottery you will gain the amount of 
+        the probability of a lottery you will gain the amount of
         Swiss Francs represented by the pile.
         There is always one pile that has a probability of 100% for payout.
         The other probability changes every 16 trials.
@@ -198,7 +203,7 @@ class CalibrationSession(PileSession):
         the second lottery, by using your index or middle finger.
         Immediately after your choice, we ask how certain you were
         about your choice from a scale from 1 (very uncertain)
-        to 4 (very certain).  
+        to 4 (very certain).
 
         This is run {run}/4.
 
@@ -207,7 +212,7 @@ class CalibrationSession(PileSession):
         """
 
         trial_settings = pd.read_csv(op.abspath(op.join('settings',
-            f'sub-{self.subject}_ses-calibrate.tsv')), sep='\t')
+                                                        f'sub-{self.subject}_ses-calibrate.tsv')), sep='\t')
 
         self.trials = []
 
@@ -218,19 +223,20 @@ class CalibrationSession(PileSession):
 
         for run, d in trial_settings.groupby(['run'], sort=False):
             self.trials.append(InstructionTrial(self, trial_nr=run,
-                txt=txt.format(run=run)))
+                                                txt=txt.format(run=run)))
             for (p1, p2), d2 in d.groupby(['p1', 'p2'], sort=False):
                 self.trials.append(IntroBlockTrial(session=self, trial_nr=run,
-                    prob1=p1,
-                    prob2=p2))
+                                                   prob1=p1,
+                                                   prob2=p2))
 
                 for ix, row in d2.iterrows():
                     self.trials.append(GambleTrial(self, row.trial,
-                        prob1=row.p1, prob2=row.p2,
-                        num1=int(row.n1), 
-                        num2=int(row.n2),
-                        jitter1=jitter1,
-                        jitter2=jitter2))
+                                                   prob1=row.p1, prob2=row.p2,
+                                                   num1=int(row.n1),
+                                                   num2=int(row.n2),
+                                                   jitter1=jitter1,
+                                                   jitter2=jitter2))
+
 
 if __name__ == '__main__':
 
