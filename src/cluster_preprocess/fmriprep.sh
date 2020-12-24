e5906abb7e80e5b39e1c519af30f15030082c2c8
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2020-12-24 19:16:42 +0100

    new version fmriprep

--- src/cluster_preprocess/fmriprep.sh
+++ src/cluster_preprocess/fmriprep.sh
@@ -7,4 +7,4 @@
 #SBATCH --time=16:00:00
 export SINGULARITYENV_FS_LICENSE=$FREESURFER_HOME/license.txt
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
-singularity run --cleanenv $HOME/fmriprep-20.2.0.simg /home/gholland/data/numerosity_7t/ds-numrisk /home/gholland/data/numerosity_7t/ds-numrisk/derivatives participant --participant-label $PARTICIPANT_LABEL -w /scratch  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --skip_bids_validation --no-submm-recon --dummy-scans 3
+singularity run --cleanenv $HOME/fmriprep-20.2.1.simg /home/gholland/data/numerosity_7t/ds-numrisk /home/gholland/data/numerosity_7t/ds-numrisk/derivatives participant --participant-label $PARTICIPANT_LABEL -w /scratch  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --skip_bids_validation --no-submm-recon --dummy-scans 3
