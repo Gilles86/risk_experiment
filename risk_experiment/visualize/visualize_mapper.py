bed6c4d94e905f5cfd11c7ce842de453127954fe
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2021-03-03 18:04:01 +0100

    New version that visualizes mapper in fsaverage-space

--- risk_experiment/visualize/visualize_mapper.py
+++ risk_experiment/visualize/visualize_mapper.py
@@ -2,6 +2,7 @@ import os.path as op
 from nilearn import surface
 import cortex
 import numpy as np
+from risk_experiment.utils.argparse import run_main, make_default_parser
 
 sourcedata = '/data2/ds-risk'
 subject = '10'
@@ -9,71 +10,89 @@ session = '3t1'
 thr = .15
 
 
-left, right = cortex.db.get_surf(subject, 'fiducial', merge=False)
-left, right = cortex.Surface(*left), cortex.Surface(*right)
+def main(subject, session, sourcedata, standard_space=False, thr=thr):
+    left, right = cortex.db.get_surf(f'sub-{subject}', 'fiducial', merge=False)
+    left, right = cortex.Surface(*left), cortex.Surface(*right)
 
 
-def smooth(data, *args, **kwargs):
-    n_left_pts = len(left.pts)
+    def smooth(data, *args, **kwargs):
+        n_left_pts = len(left.pts)
 
-    smoothed_data = np.zeros_like(data)
-    smoothed_data[:n_left_pts] = left.smooth(data[:n_left_pts], *args, **kwargs)
-    smoothed_data[n_left_pts:] = right.smooth(data[n_left_pts:], *args, **kwargs)
+        smoothed_data = np.zeros_like(data)
+        smoothed_data[:n_left_pts] = left.smooth(data[:n_left_pts], *args, **kwargs)
+        smoothed_data[n_left_pts:] = right.smooth(data[n_left_pts:], *args, **kwargs)
 
-    return smoothed_data
-
-
-def _load_parameters(subject, session, par, smoothed=False, concatenated=False, alpha=None):
+        return smoothed_data
 
 
-    d_name = 'encoding_model'
-    if smoothed:
-        d_name += '.smoothed'
-    if concatenated:
-        d_name += '.concatenated'
-
-    dir_ = op.join(sourcedata, 'derivatives', d_name, f'sub-{subject}',
-            f'ses-{session}', 'func')
-
-    print(dir_)
-
-    par_l = op.join(dir_, f'sub-{subject}_ses-{session}_desc-{par}_space-fsnative_hemi-L.func.gii')
-    par_r = op.join(dir_, f'sub-{subject}_ses-{session}_desc-{par}_space-fsnative_hemi-R.func.gii')
-
-    par_l = surface.load_surf_data(par_l).T
-    par_r = surface.load_surf_data(par_r).T
-
-    par = np.concatenate((par_l, par_r))
-
-    return cortex.Vertex(par, subject, alpha=alpha)
+    def _load_parameters(subject, session, par, space='fsnative', smoothed=False, concatenated=False, alpha=None):
 
 
-d = {}
-for session in ['3t1', '7t1'][1:2]:
-    for par in ['r2.grid', 'r2.optim', 'mu.grid', 'mu.optim', 'sd.grid', 'sd.optim']:
-        for smoothed in [True]:
-            for concatenated in [False, True]:
-                key = f'{session}.{par}'
+        d_name = 'encoding_model'
+        if smoothed:
+            d_name += '.smoothed'
+        if concatenated:
+            d_name += '.concatenated'
 
-                if smoothed:
-                    key += '.smoothed'
-                if concatenated:
-                    key += '.concatenated'
-                         
+        dir_ = op.join(sourcedata, 'derivatives', d_name, f'sub-{subject}',
+                f'ses-{session}', 'func')
 
-                if par.startswith('mu') or par.startswith('sd'):
-                    pass
-                    values = _load_parameters(subject, session, par, smoothed,
-                            concatenated=concatenated)
-                            # alpha=d[f'{session}.r2.optim.smoothed'])
-                    # values.data[d[f'{session}.r2.optim.smoothed.concatenated'].data < thr] = np.nan
-                else:
-                    values = _load_parameters(subject, session, par, smoothed,
-                            concatenated=concatenated)
-                # elif par.startswith('r2'):
-                    # d[key + '.smoothed'] = cortex.Vertex(smooth(values.data), subject, factor=3.0, iterations=1)
+        print(dir_)
 
-                d[key] = values
+        par_l = op.join(dir_, f'sub-{subject}_ses-{session}_desc-{par}_space-{space}_hemi-L.func.gii')
+        par_r = op.join(dir_, f'sub-{subject}_ses-{session}_desc-{par}_space-{space}_hemi-R.func.gii')
 
-ds = cortex.Dataset(**d)
-cortex.webshow(d)
+        par_l = surface.load_surf_data(par_l).T
+        par_r = surface.load_surf_data(par_r).T
+
+        par = np.concatenate((par_l, par_r))
+
+        if space == 'fsnative':
+            fs_subject = f'sub-{subject}'
+        else:
+            fs_subject = space
+
+        return cortex.Vertex(par, fs_subject, alpha=alpha)
+
+
+    d = {}
+
+    if standard_space:
+        space = 'fsaverage'
+    else:
+        space = 'fsnative'
+
+    for session in ['3t1', '7t1'][:2]:
+        for par in ['r2.grid', 'r2.optim', 'mu.grid', 'mu.optim', 'sd.grid', 'sd.optim', 'amplitude.optim']:
+            for smoothed in [True]:
+                for concatenated in [False]:
+                    key = f'{session}.{par}'
+
+                    if smoothed:
+                        key += '.smoothed'
+                    if concatenated:
+                        key += '.concatenated'
+                             
+
+                    if par.startswith('mu') or par.startswith('sd'):
+                        pass
+                        values = _load_parameters(subject, session, par, 
+                                space, smoothed,
+                                concatenated=concatenated)
+                                # alpha=d[f'{session}.r2.optim.smoothed'])
+                        values.data[d[f'{session}.r2.optim.smoothed'].data < thr] = np.nan
+                    else:
+                        values = _load_parameters(subject, session, par,
+                                space, smoothed,
+                                concatenated=concatenated)
+
+                    d[key] = values
+
+    ds = cortex.Dataset(**d)
+    cortex.webshow(ds)
+
+if __name__ == '__main__':
+    parser = make_default_parser()
+    parser.add_argument('--standard_space', action='store_true')
+    args = parser.parse_args()
+    main(args.subject, args.session, args.bids_folder, standard_space=args.standard_space)
