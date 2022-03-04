b3e6c017ed40904cf8b76a8792e4840139ec43ce
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-03-04 16:14:43 +0100

    First version of nPRF split for certainties

--- risk_experiment/encoding_model/fit_task_surf.py
+++ risk_experiment/encoding_model/fit_task_surf.py
@@ -13,7 +13,7 @@ import seaborn as sns
 
 
 def main(subject, session, bids_folder='/data/ds-risk', smoothed=False,
-        pca_confounds=False):
+        pca_confounds=False, split_certainty=True):
 
     key = 'glm_stim1_surf'
     target_dir = 'encoding_model'
@@ -26,47 +26,61 @@ def main(subject, session, bids_folder='/data/ds-risk', smoothed=False,
         target_dir += '.pca_confounds'
         key += '.pca_confounds'
 
+    if split_certaity:
+        target_dir += 'split_certainty'
+
     target_dir = get_target_dir(subject, session, bids_folder, target_dir)
 
     paradigm = [pd.read_csv(op.join(bids_folder, f'sub-{subject}', f'ses-{session}',
                                'func', f'sub-{subject}_ses-{session}_task-task_run-{run}_events.tsv'), sep='\t')
                 for run in range(1, 9)]
     paradigm = pd.concat(paradigm, keys=range(1,9), names=['run'])
-    paradigm = paradigm[paradigm.trial_type == 'stimulus 1'][['n1', 'trial_nr']].set_index('trial_nr')
+    paradigm = paradigm[paradigm.trial_type == 'stimulus 1'].set_index('trial_nr')
+
+    if split_certainty:
+        ixs = [paradigm['prob1'] == 0.55, paradigm['prob1'] == 1.0]
+        spit_keys = ['uncertain', 'certain']
+    else:
+        ixs = [paradigm.index]
+        spit_keys = ['']
+
+    paradigm = paradigm[['n1']]
     paradigm['n1'] = np.log(paradigm['n1'])
+
+    model = GaussianPRF()
+    # SET UP GRID
+    mus = np.log(np.linspace(5, 80, 100, dtype=np.float32))
+    sds = np.log(np.linspace(2, 30, 100, dtype=np.float32))
+    amplitudes = np.array([1.], dtype=np.float32)
+    baselines = np.array([0], dtype=np.float32)
 
     for hemi in ['L', 'R']:
 
         data = surface.load_surf_data(op.join(bids_folder, 'derivatives', key,
                                               f'sub-{subject}', f'ses-{session}', 'func', f'sub-{subject}_ses-{session}_task-task_space-fsnative_desc-stims1_hemi-{hemi}.pe.gii')).T
 
-        model = GaussianPRF()
-        # SET UP GRID
-        mus = np.log(np.linspace(5, 80, 100, dtype=np.float32))
-        sds = np.log(np.linspace(2, 30, 100, dtype=np.float32))
-        amplitudes = np.array([1.], dtype=np.float32)
-        baselines = np.array([0], dtype=np.float32)
 
-        optimizer = ParameterFitter(model, data, paradigm)
+        for ix, split_key in zip(ixs, split_keys):
+            optimizer = ParameterFitter(model, data.loc[ix], paradigm.loc[ix])
 
-        grid_parameters = optimizer.fit_grid(mus, sds, amplitudes, baselines, use_correlation_cost=True)
-        grid_parameters = optimizer.refine_baseline_and_amplitude(grid_parameters, n_iterations=2)
+            grid_parameters = optimizer.fit_grid(mus, sds, amplitudes, baselines, use_correlation_cost=True)
+            grid_parameters = optimizer.refine_baseline_and_amplitude(grid_parameters, n_iterations=2)
 
 
-        optimizer.fit(init_pars=grid_parameters, learning_rate=.05, store_intermediate_parameters=False, max_n_iterations=10000,
-                r2_atol=0.00001)
+            optimizer.fit(init_pars=grid_parameters, learning_rate=.05, store_intermediate_parameters=False, max_n_iterations=10000,
+                    r2_atol=0.00001)
 
-        target_fn = op.join(target_dir, f'sub-{subject}_ses-{session}_desc-r2.optim_space-fsnative_hemi-{hemi}.func.gii')
-        write_gifti(subject, session, bids_folder, 'fsnative', 
-                pd.concat([optimizer.r2], keys=[hemi], names=['hemi']), target_fn)
-        transform_data(target_fn, f'sub-{subject}', bids_folder, target_subject='fsaverage')
-
-        for par, values in optimizer.estimated_parameters.T.iterrows():
-            print(values)
-            target_fn = op.join(target_dir, f'sub-{subject}_ses-{session}_desc-{par}.optim_space-fsnative_hemi-{hemi}.func.gii')
+            target_fn = op.join(target_dir, f'sub-{subject}_ses-{session}_desc-r2.optim_space-fsnative_hemi-{hemi}.func.gii')
             write_gifti(subject, session, bids_folder, 'fsnative', 
-                    pd.concat([values], keys=[hemi], names=['hemi']), target_fn)
+                    pd.concat([optimizer.r2], keys=[hemi], names=['hemi']), target_fn)
             transform_data(target_fn, f'sub-{subject}', bids_folder, target_subject='fsaverage')
+
+            for par, values in optimizer.estimated_parameters.T.iterrows():
+                print(values)
+                target_fn = op.join(target_dir, f'sub-{subject}_ses-{session}_desc-{par}.optim{split_key}_space-fsnative_hemi-{hemi}.func.gii')
+                write_gifti(subject, session, bids_folder, 'fsnative', 
+                        pd.concat([values], keys=[hemi], names=['hemi']), target_fn)
+                transform_data(target_fn, f'sub-{subject}', bids_folder, target_subject='fsaverage')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
