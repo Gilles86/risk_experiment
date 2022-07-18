d8edff6dc0f49622543867e4bcda599f0638ce2f
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2022-07-18 18:16:58 +0200

    Certainty evidence model

--- risk_experiment/hbmodels/fit_evidence_model.py
+++ risk_experiment/hbmodels/fit_evidence_model.py
@@ -2,17 +2,28 @@ import os
 import os.path as op
 import argparse
 from risk_experiment.utils.data import get_all_behavior
-from evidence_model import EvidenceModel
+from evidence_model import EvidenceModel, EvidenceModelRegression
 import arviz as az
 
 
 def main(model, session='7t2', bids_folder='/data'):
 
-    if model != 'model1':
+    if model not in ['model1', 'certainty']:
         raise NotImplementedError
 
     df = get_all_behavior(sessions=session, bids_folder=bids_folder)
-    model = EvidenceModel(df)
+    
+    if model == 'model1':
+        model = EvidenceModel(df)
+    elif model == 'certainty':
+        from scipy.stats import zscore
+
+        df['z_certainty'] = df.groupby(['subject']).certainty.apply(zscore)
+        df['z_certainty'] = df['z_certainty'].fillna(0.0)
+
+        model = EvidenceModelRegression(df, regressors={'evidence_sd1':'z_certainty',
+                                                        'evidence_sd2':'z_certainty'})
+
     model.build_model()
     trace = model.sample(500, 500)
 
@@ -20,7 +31,7 @@ def main(model, session='7t2', bids_folder='/data'):
     if not op.exists(target_folder):
         os.makedirs(target_folder)
         
-    az.to_netcdf(trace,  op.join(target_folder, f'evidence_ses-{session}_model-1.nc'))
+    az.to_netcdf(trace,  op.join(target_folder, f'evidence_ses-{session}_model-{model}.nc'))
 
 
 if __name__ == '__main__':
