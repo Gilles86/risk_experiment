a69779d99825a64c0e64e6890ff98123cb799cad
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-10-20 09:38:44 +0200

    Better filenames

--- risk_experiment/glms/fit_single_trials_volume_denoise.py
+++ risk_experiment/glms/fit_single_trials_volume_denoise.py
@@ -23,14 +23,13 @@ def main(subject, session, bids_folder, smoothed=False, retroicor=False):
 
     base_dir = 'glm_stim1.denoise'
 
-    if smoothed:
-        base_dir += '.smoothed'
-        ims = [image.smooth_img(im, fwhm=5.0) for im in ims]
-        confounds = sub.get_retroicor_confounds(session)
-
     if retroicor:
         base_dir += '.retroicor'
         confounds = sub.get_retroicor_confounds(session)
+
+    if smoothed:
+        base_dir += '.smoothed'
+        ims = [image.smooth_img(im, fwhm=5.0) for im in ims]
 
     data = [image.load_img(im).get_fdata() for im in ims]
 
@@ -44,6 +43,7 @@ def main(subject, session, bids_folder, smoothed=False, retroicor=False):
     print(onsets)
     tr = 2.3
     n = 160
+
     frametimes = np.linspace(tr/2., (n - .5)*tr, n)
     onsets['onset'] = ((onsets['onset']+tr/2.) // 2.3) * 2.3
 
@@ -73,7 +73,8 @@ def main(subject, session, bids_folder, smoothed=False, retroicor=False):
     opt['wantfileoutputs'] = [0, 0, 0, 1]
 
     if retroicor:
-        opt['extra_regressors'] = [cf.values for cf in confounds]
+        if np.all([cf.shape[1] > 0 for cf in confounds]):
+            opt['extra_regressors'] = [cf.values[:n, :] for cf in confounds]
 
     # running python GLMsingle involves creating a GLM_single object
     # and then running the procedure using the .fit() routine
