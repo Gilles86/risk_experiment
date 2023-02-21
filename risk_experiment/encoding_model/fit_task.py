09c26912f32d1d19bfd95aa4cab25a5198cf9c8d
Gilles de Hollander <gilles.de.hollander@gmail.com>
2023-02-21 10:36:12 +0100

    Cleanup

--- risk_experiment/encoding_model/fit_task.py
+++ risk_experiment/encoding_model/fit_task.py
@@ -1,9 +1,8 @@
-
 import argparse
 import pandas as pd
 from braincoder.models import GaussianPRF
 from braincoder.optimize import ParameterFitter
-from risk_experiment.utils import get_surf_data, get_mapper_paradigm, write_gifti, get_target_dir
+from risk_experiment.utils import get_target_dir
 from risk_experiment.utils.surface import transform_data
 from nilearn import surface
 from nilearn.input_data import NiftiMasker
